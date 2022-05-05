bc2075f4b1e99c1e01a735c3d6005cd206adf58c
Szarvas Péter <peterszarvas94@gmail.com>
2022-05-05 23:53:41 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import CouplePicture from './components/CouplePicture';
+
+function App() {
+  return (
+    <div className="App">
+      <CouplePicture/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CouplePicture.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/couplepicture.css";
+import "../styles/font.css";
+
+const CouplePicture = () => {
+	return (
+		<>
+			<div className='couplepicture'/>
+			<h1 className="roseville maintitle">Adri &amp; Peti</h1>
+		</>
+	);
+};
+
+export default CouplePicture;
